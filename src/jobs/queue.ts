469be1d86ad5bdcdba0d6b0249834fc78921389f
ArkojeetSuntel <111523962+ArkojeetSuntel@users.noreply.github.com>
2025-04-30 17:17:26 +0530

    Making smaller changes to commit

--- src/jobs/queue.ts
+++
@@ -1,6 +0,0 @@
-import { Queue } from 'bullmq';
-import { RedisOptions } from 'ioredis';
-
-const connection: RedisOptions = { host: 'localhost', port: 6379 };
-
-export const renderQueue = new Queue('render', { connection });
